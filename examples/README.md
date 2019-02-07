e082c18a1fbbd020b2a5f36712919206c8efb614
Nikolay Nikolaev <nnikolay@vmware.com>
2019-02-07 09:48:10 +0200

    mk/targets.mk: support AUX_CONTAINERS and multiple examples
    Adding AUX_CONATINERS for example Makefiles. This allows inter-example
    container dependency.
    
    Use define and eval to generate the targets in mk/targets.mk to enable
    the independent example generation and avoid global variable overwrite.
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- examples/README.md
+++ examples/README.md
@@ -5,6 +5,7 @@ To add a new example, one need to create its folder under `examples` and adda `M
 ```Makefile
 NAME = <example name>
 CONTAINERS = <container 1> <container 2> <container 3>
+AUX_CONTAINERS = <aux container 1> <aux container 2>
 PODS = <pod 1> <pod 2>
 CHECK = <a command to check the operability of the deployed example>
 
@@ -13,6 +14,7 @@ include $(TOP)/mk/targets.mk
 
  * `NAME` - is the name of the example, e.g. `proxy`
  * `CONTAINERS` - is a list of all the containers to be built. Each of these should have a folder under `examples/<NAME>`, e.g. `examples/proxy/nginx`. This folder should have a file called `Dockerfile` to decribe how the container is built. The `Dockerfile` assumes to be executed from the top level project folder, i.e. the paths refernced in it look like `examples/proxy/nginx/html`.
+ * `AUX_CONTAINERS` - specify a dependency container from another example. This allows for example to share container build definitions. The container name has to be in the form of `<DEP_NAME>-<DEP_CONTAINER>`, where `<DEP_NAME>` is the name of th dependency example and `<DEP_CONTAINER>` is the name of the contanier within that example.
  * `PODS` - the list of the pods to be deployed. Each of the names here shod correspond to a file in the example's `k8s` folder. E.g. `examples/proxy/k8s/proxy-nsc.yaml`
  * `CHECK` - a command to be executed to verify the operability of the deployment. The command is executed from the examples folder. It assumes all paths are relative to it.
 
--- mk/targets.mk
+++ mk/targets.mk
@@ -2,28 +2,45 @@ PREFIX = k8s
 
 include $(TOP)/mk/docker-targets.mk
 
-.PHONY: ${PREFIX}-${NAME}-build
-${PREFIX}-${NAME}-build: $(addsuffix -build,$(addprefix ${CONTAINER_BUILD_PREFIX}-$(NAME)-,$(CONTAINERS)))
+define BUILD
+.PHONY: $(PREFIX)-$(NAME)-build
+$(PREFIX)-$(NAME)-build: $(addsuffix -build,$(addprefix ${CONTAINER_BUILD_PREFIX}-$(NAME)-,$(CONTAINERS)))
+endef
+$(eval $(BUILD))
 
-.PHONY: ${PREFIX}-${NAME}-save
-${PREFIX}-${NAME}-save: $(addsuffix -save,$(addprefix ${CONTAINER_BUILD_PREFIX}-$(NAME)-,$(CONTAINERS)))
+define SAVE
+.PHONY: $(PREFIX)-$(NAME)-save
+$(PREFIX)-$(NAME)-save: $(addsuffix -save,$(addprefix ${CONTAINER_BUILD_PREFIX}-$(NAME)-,$(CONTAINERS))) $(addsuffix -save,$(addprefix ${CONTAINER_BUILD_PREFIX}-,$(AUX_CONTAINERS)))
+endef
+$(eval $(SAVE))
 
-.PHONY: ${PREFIX}-${NAME}-load-images
-${PREFIX}-${NAME}-load-images:  $(addsuffix -load-images,$(addprefix $(CLUSTER_RULES_PREFIX)-${NAME}-,$(CONTAINERS)))
+define LOAD_IMAGES
+.PHONY: $(PREFIX)-$(NAME)-load-images
+$(PREFIX)-$(NAME)-load-images:  $(addsuffix -load-images,$(addprefix $(CLUSTER_RULES_PREFIX)-$(NAME)-,$(CONTAINERS))) $(addsuffix -load-images,$(addprefix $(CLUSTER_RULES_PREFIX)-,$(AUX_CONTAINERS)))
+endef
+$(eval $(LOAD_IMAGES))
 
-.PHONY: ${PREFIX}-${NAME}-deploy
-${PREFIX}-${NAME}-deploy: ${PREFIX}-${NAME}-delete ${PREFIX}-${NAME}-load-images $(addsuffix -deploy,$(addprefix $(PREFIX)-${NAME}-,$(PODS)))
+define DEPLOY
+.PHONY: $(PREFIX)-$(NAME)-deploy
+$(PREFIX)-$(NAME)-deploy: $(PREFIX)-$(NAME)-delete $(PREFIX)-$(NAME)-load-images $(addsuffix -deploy,$(addprefix $(PREFIX)-$(NAME)-,$(PODS)))
 
-.PHONY: ${PREFIX}-${NAME}-%-deploy
-${PREFIX}-${NAME}-%-deploy:
-	@until ! $$(kubectl get pods | grep -q ^$* ); do echo "Wait for $* to terminate"; sleep 1; done
-	@sed "s;\(image:[ \t]*networkservicemesh/[^:]*\).*;\1$${COMMIT/$${COMMIT}/:$${COMMIT}};" examples/${NAME}/${PREFIX}/$*.yaml | kubectl apply -f -
+.PHONY: $(PREFIX)-$(NAME)-%-deploy
+$(PREFIX)-$(NAME)-%-deploy:
+	@sed "s;\(image:[ \t]*networkservicemesh/[^:]*\).*;\1$${COMMIT/$${COMMIT}/:$${COMMIT}};" examples/$(NAME)/$(PREFIX)/$*.yaml | kubectl apply -f -
+endef
+$(eval $(DEPLOY))
 
-.PHONY: ${PREFIX}-${NAME}-delete
-${PREFIX}-${NAME}-delete:
-	@echo "Deleting examples/${NAME}/${PREFIX}/"
-	@kubectl delete -R -f examples/${NAME}/${PREFIX}/ > /dev/null 2>&1 || echo "$* does not exist and thus cannot be deleted"
+define DELETE
+.PHONY: $(PREFIX)-$(NAME)-delete
+$(PREFIX)-$(NAME)-delete:
+	@echo "Deleting examples/$(NAME)/$(PREFIX)/"
+	@kubectl delete -R -f examples/$(NAME)/$(PREFIX)/ > /dev/null 2>&1 || echo "$* does not exist and thus cannot be deleted"
+endef
+$(eval $(DELETE))
 
-.PHONY: ${PREFIX}-${NAME}-check
-${PREFIX}-${NAME}-check:
-	@cd examples/${NAME} && ${CHECK}
+define RUN_CHECK
+.PHONY: $(PREFIX)-$(NAME)-check
+$(PREFIX)-$(NAME)-check:
+	@cd examples/$(NAME) && $(CHECK)
+endef
+$(eval $(RUN_CHECK))
