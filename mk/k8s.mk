f9ae006fb3220f71b46f02702b6b9a5f558d5283
Nikolay Nikolaev <nnikolay@vmware.com>
2019-03-12 09:44:55 +0200

    Add cluster provider kind.sigs.k8s.io
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- mk/k8s.mk
+++ mk/k8s.mk
@@ -26,6 +26,11 @@ CLUSTER_RULES_PREFIX := vagrant
 endif
 include mk/vagrant.mk
 
+# .kind.mk enables the kind.sigs.k8s.io docker based K8s install:
+# export CLUSTER_RULES_PREFIX=kind
+# before running make
+include mk/kind.mk
+
 # .null.mk allows you to skip the vagrant machinery with:
 # export CLUSTER_RULES_PREFIX=null
 # before running make
