da9c1c97a3ee20aedd6031b4a88a8c8bb0652aa9
Nikolay Nikolaev <nnikolay@vmware.com>
2019-05-11 02:06:16 +0300

    envoy: add the basic example
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

---
+++ examples/envoy_echo/envoy-nse/cmd/nse.go
@@ -0,0 +1,44 @@
+// Copyright 2019 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/networkservicemesh/networkservicemesh/pkg/tools"
+	"github.com/networkservicemesh/networkservicemesh/sdk/endpoint"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	// Capture signals to cleanup before exiting
+	c := tools.NewOSSignalChannel()
+
+	composite := endpoint.NewCompositeEndpoint(
+		endpoint.NewMonitorEndpoint(nil),
+		endpoint.NewIpamEndpoint(nil),
+		endpoint.NewConnectionEndpoint(nil))
+
+	nsmEndpoint, err := endpoint.NewNSMEndpoint(nil, nil, composite)
+	if err != nil {
+		logrus.Fatalf("%v", err)
+	}
+
+	_ = nsmEndpoint.Start()
+	defer func() { _ = nsmEndpoint.Delete() }()
+
+	// Capture signals to cleanup before exiting
+	<-c
+}
