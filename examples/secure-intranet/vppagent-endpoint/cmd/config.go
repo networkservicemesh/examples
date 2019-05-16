b5ef88d293ad31df418a38c8af983ddc2a2e6c72
Nikolay Nikolaev <nnikolay@vmware.com>
2019-05-16 14:23:07 +0300

    Add seccure-intranet example
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

---
+++ examples/secure-intranet/vppagent-endpoint/cmd/config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+const (
+	aclRules = "aclRules"
+)
+
+var viperConfig *viper.Viper
+
+func initConfig() {
+	viperConfig = viper.New()
+	viperConfig.SetConfigName("config")
+	viperConfig.AddConfigPath("/etc/vppagent-acl-filter/")
+
+	if err := viperConfig.ReadInConfig(); err == nil {
+		viperConfig.WatchConfig()
+		viperConfig.OnConfigChange(func(e fsnotify.Event) {
+			fmt.Println("Config file changed:", e.Name)
+		})
+	} else {
+		logrus.Errorf("Error reading the config file: %s \n", err)
+	}
+
+	logrus.Infof("ACL filter config finished")
+}
+
+func getAclRulesConfig() map[string]string {
+	return viperConfig.GetStringMapString(aclRules)
+}
