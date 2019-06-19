ebb73fb7fae3efd0996a727e6d3d161381298c05
Nikolay Nikolaev <nnikolay@vmware.com>
2019-06-19 16:58:41 +0300

    Add bridge-domain
    NOT WORKING yet
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

---
+++ examples/bridge-domain/bridge/cmd/interface.go
@@ -0,0 +1,104 @@
+// Copyright 2019 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"path"
+
+	"github.com/ligato/vpp-agent/api/configurator"
+	vpp "github.com/ligato/vpp-agent/api/models/vpp"
+	interfaces "github.com/ligato/vpp-agent/api/models/vpp/interfaces"
+	l2 "github.com/ligato/vpp-agent/api/models/vpp/l2"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/local/connection"
+	"github.com/sirupsen/logrus"
+)
+
+func (vxc *vppAgentBridgeComposite) insertVPPAgentInterface(conn *connection.Connection,
+	connect bool, baseDir string) error {
+
+	ifName := "client-" + conn.GetId()
+
+	SocketDir := path.Dir(path.Join(baseDir, conn.GetMechanism().GetSocketFilename()))
+	if err := os.MkdirAll(SocketDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	var bd *l2.BridgeDomain
+
+	if connect {
+		vxc.bdInterfaces = append(vxc.bdInterfaces, &l2.BridgeDomain_Interface{
+			Name:                    ifName,
+			BridgedVirtualInterface: false,
+		})
+
+		bd = &l2.BridgeDomain{
+			Name:                "bd1",
+			Flood:               false,
+			UnknownUnicastFlood: false,
+			Forward:             true,
+			Learn:               true,
+			ArpTermination:      false,
+			Interfaces:          vxc.bdInterfaces,
+		}
+	} else {
+		bd = &l2.BridgeDomain{
+			Name:                "bd1",
+			Flood:               false,
+			UnknownUnicastFlood: false,
+			Forward:             true,
+			Learn:               true,
+			ArpTermination:      false,
+			Interfaces: []*l2.BridgeDomain_Interface{
+				{
+					Name:                    ifName,
+					BridgedVirtualInterface: false,
+				},
+			},
+		}
+	}
+
+	dataChange := &configurator.Config{
+		VppConfig: &vpp.ConfigData{
+			Interfaces: []*interfaces.Interface{
+				{
+					Name:    ifName,
+					Type:    interfaces.Interface_MEMIF,
+					Enabled: true,
+					Link: &interfaces.Interface_Memif{
+						Memif: &interfaces.MemifLink{
+							Master:         true,
+							SocketFilename: path.Join(baseDir, conn.GetMechanism().GetSocketFilename()),
+						},
+					},
+				},
+			},
+			BridgeDomains: []*l2.BridgeDomain{
+				bd,
+			},
+		},
+	}
+
+	logrus.Infof("Sending DataChange to vppagent: %+v", dataChange)
+
+	err := sendDataChangeToVppAgent(dataChange, connect)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+	return nil
+}
---
+++ examples/bridge-domain/bridge/cmd/ipam.go
@@ -0,0 +1,137 @@
+// Copyright 2018, 2019 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/connectioncontext"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/local/connection"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/local/networkservice"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/prefix_pool"
+	"github.com/networkservicemesh/networkservicemesh/sdk/common"
+	"github.com/networkservicemesh/networkservicemesh/sdk/endpoint"
+	"github.com/sirupsen/logrus"
+)
+
+type IpamEndpoint struct {
+	endpoint.BaseCompositeEndpoint
+	PrefixPool prefix_pool.PrefixPool
+	SelfIP     string
+}
+
+// Request implements the request handler
+func (ice *IpamEndpoint) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*connection.Connection, error) {
+
+	if ice.GetNext() == nil {
+		err := fmt.Errorf("IPAM needs next")
+		logrus.Errorf("%v", err)
+		return nil, err
+	}
+
+	newConnection, err := ice.GetNext().Request(ctx, request)
+	if err != nil {
+		logrus.Errorf("Next request failed: %v", err)
+		return nil, err
+	}
+
+	/* Exclude the prefixes from the pool of available prefixes */
+	excludedPrefixes, err := ice.PrefixPool.ExcludePrefixes(request.Connection.Context.ExcludedPrefixes)
+	if err != nil {
+		return nil, err
+	}
+
+	/* Determine whether the pool is IPv4 or IPv6 */
+	currentIPFamily := connectioncontext.IpFamily_IPV4
+	if common.IsIPv6(ice.PrefixPool.GetPrefixes()[0]) {
+		currentIPFamily = connectioncontext.IpFamily_IPV6
+	}
+
+	srcIP, dstIP, prefixes, err := ice.PrefixPool.Extract(request.Connection.Id, currentIPFamily, request.Connection.Context.ExtraPrefixRequest...)
+	if err != nil {
+		return nil, err
+	}
+
+	/* Release the actual prefixes that were excluded during IPAM */
+	err = ice.PrefixPool.ReleaseExcludedPrefixes(excludedPrefixes)
+	if err != nil {
+		return nil, err
+	}
+
+	// We'll use same DST IP for all connections
+	if len(ice.SelfIP) == 0 {
+		ice.SelfIP = dstIP.String()
+	}
+
+	// Update source/dst IP's
+	newConnection.Context.SrcIpAddr = srcIP.String()
+	newConnection.Context.DstIpAddr = ice.SelfIP
+
+	newConnection.Context.ExtraPrefixes = prefixes
+
+	err = newConnection.IsComplete()
+	if err != nil {
+		logrus.Errorf("New connection is not complete: %v", err)
+		return nil, err
+	}
+
+	logrus.Infof("IPAM completed on connection: %v", newConnection)
+	return newConnection, nil
+}
+
+// Close implements the close handler
+func (ice *IpamEndpoint) Close(ctx context.Context, connection *connection.Connection) (*empty.Empty, error) {
+	prefix, requests, err := ice.PrefixPool.GetConnectionInformation(connection.GetId())
+	logrus.Infof("Release connection prefixes network: %s extra requests: %v", prefix, requests)
+	if err != nil {
+		logrus.Errorf("Error: %v", err)
+	}
+	err = ice.PrefixPool.Release(connection.GetId())
+	if err != nil {
+		logrus.Error("Release error: ", err)
+	}
+	if ice.GetNext() != nil {
+		return ice.GetNext().Close(ctx, connection)
+	}
+	return &empty.Empty{}, nil
+}
+
+// NewIpamEndpoint creates a IpamEndpoint
+func NewIpamEndpoint(configuration *common.NSConfiguration) *IpamEndpoint {
+	// ensure the env variables are processed
+	if configuration == nil {
+		configuration = &common.NSConfiguration{}
+	}
+	configuration.CompleteNSConfiguration()
+
+	pool, err := prefix_pool.NewPrefixPool(configuration.IPAddress)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	self := &IpamEndpoint{
+		PrefixPool: pool,
+		SelfIP:     "",
+	}
+
+	return self
+}
