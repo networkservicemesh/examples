4dd6ba78f6c9d41e8169068c08b8de6d93a7d309
Nikolay Nikolaev <nnikolay@vmware.com>
2019-06-21 01:12:41 +0300

    Set custom ORG in docker push
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- mk/docker-targets.mk
+++ mk/docker-targets.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ORG=networkservicemesh
+ORG?=networkservicemesh
 
 # Setup proxies for docker build
 ifeq ($(HTTP_PROXY),)
@@ -27,7 +27,7 @@ else
 HTTPSBUILD=--build-arg HTTPS_PROXY=$(HTTPS_PROXY)
 endif
 
-DOCKERBUILD=docker build --network="host" --build-arg VPP_AGENT=${VPP_AGENT}  ${HTTPBUILD} ${HTTPSBUILD}
+DOCKERBUILD=docker build --network="host" --build-arg VPP_AGENT=${VPP_AGENT} ${HTTPBUILD} ${HTTPSBUILD}
 
 define generate-docker-targets
 .PHONY: docker-$1-$2-build
