405b8bcc75157691cd3e61f03fd8862f40555e5e
Nikolay Nikolaev <nnikolay@vmware.com>
2019-07-27 18:15:07 +0300

    The description is moved in the README for the proxy nsc
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- examples/proxy/proxy-nsc/cmd/nsc.go
+++ examples/proxy/proxy-nsc/cmd/nsc.go
@@ -33,24 +33,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-/*
-	The example proxy-nsc is an implementation of HTTP proxy as NS Client.
-	It can be use as an NS ingress proxy, where the proxy service is exposed as an
-	outside facing service. Upon HTTP connection request the proxy will create
-	a new NS connection. The connection is configured by env variables as an usual
-	NS Client. The proxy will scan the HTTP request for headers of format:
-		NSM-Lable:Value
-	These would be transformed to NS Client request labels.
-
-                       +------------+                      +-------------+
-  GET / HTTP/1.1       |            |                      |             |
-  NSM-App: Firewall    |            |     app=firewall     |             |
-+----------------------> Proxy NSC  +----------------------> NS Endopint |
-                       |            |                      |             |
-                       |            |                      |             |
-                       +------------+                      +-------------+
-*/
-
 const (
 	proxyHostEnv      = "PROXY_HOST"
 	defaultProxyHost  = ":8080"
