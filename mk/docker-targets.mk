c56f2cd7768ef364cb4911c827f7abc8c0a5089c
Tim Swanson <tiswanso@cisco.com>
2019-08-16 17:13:10 -0400

    Allow setting of TAG in docker make

--- mk/docker-targets.mk
+++ mk/docker-targets.mk
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ORG?=networkservicemesh
+ORG ?= networkservicemesh
+TAG ?= latest
 
 # Setup proxies for docker build
 ifeq ($(HTTP_PROXY),)
