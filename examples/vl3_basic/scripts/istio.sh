dd1f01b2c8b6cda732b41c11b5224920a44a622e
john-a-joyce <joycej@cisco.com>
2019-10-08 20:20:56 +0000

    Some fixes based on testing

--- examples/vl3_basic/scripts/istio.sh
+++ examples/vl3_basic/scripts/istio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Inputs: 
-# KCONF = the K8S cluster to install Istio, Istio CNI and the Pod watcher.  
+# Inputs:
+# KCONF = the K8S cluster to install Istio, Istio CNI and the Pod watcher.
 # Also the cluster that is watched
 # KCONF_KIND = the kubeconfig for the KIND cluster.  Needs to be modified from
 # what KIND automatically creates.
@@ -12,8 +12,7 @@ TAG=${TAG:-release-1.3}
 INSTALL_OP=${INSTALL_OP:-apply}
 
 sdir=$GOPATH/src
-#sdir=$GOPATH/src/istio.io
-sdir=$(dirname ${0})
+#sdir=$(dirname ${0})
 #echo "$sdir"
 
 ISTIOCNIDIR=${ISTIOCNIDIR:-${sdir}/istio.io/cni}
@@ -29,21 +28,22 @@ helm template ${ISTIOCNIDIR}/deployments/kubernetes/install/helm/istio-cni --nam
 
 echo "------------Installing Istio -----------"
 if [ ${INSTALL_OP} == "delete" ]; then
-helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=prxy_debug --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+#helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=proxy_debug --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=proxy_debug --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
 helm template ${ISTIODIR}/install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+kubectl label namespace default istio-injection="" --overwrite ${KCONF:+--kubeconfig $KCONF}
 kubectl delete namespace istio-system ${KCONF:+--kubeconfig $KCONF}
-else 
+else
 kubectl create namespace istio-system ${KCONF:+--kubeconfig $KCONF}
 sleep 3s
 helm template ${ISTIODIR}/install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
 sleep 10s
-helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=prxy_debug --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=proxy_debug --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+#helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=proxy_debug --set sidecarInjectorWebhook.enabled=false --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+#helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set proxy=proxy_debug --set sidecarInjectorWebhook.enableNamespacesByDefault=true --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+#helm template ${ISTIODIR}/install/kubernetes/helm/istio --name istio --namespace istio-system --set istio_cni.enabled=true | kubectl ${INSTALL_OP} ${KCONF:+--kubeconfig $KCONF} -f -
+kubectl label namespace default istio-injection=enabled --overwrite ${KCONF:+--kubeconfig $KCONF}
 fi
-
-echo "------------Installing NSM --------"
-source examples/vl3_basic/scripts/nsm_install_interdomain.sh
-echo "------------Installing NSC & NSE --------"
-source examples/vl3_basic/scripts/vl3_interdomain.sh
 
 echo "------------Installing pod watcher --------"
 if [ ${INSTALL_OP} == "delete" ]; then
@@ -58,9 +58,12 @@ CLUSTER_NAME=$(kubectl config view --minify=true -o jsonpath='{.clusters[].name}
 NAMESPACE=istio-system
 
 if [ ${INSTALL_OP} == "delete" ]; then
-kubectl delete secret ${CLUSTER_NAME} -n ${NAMESPACE}
+#ALREADY deleted with namespace kubectl delete secret ${CLUSTER_NAME} -n ${NAMESPACE}
+echo " ------ secret already deleted with namespace --------"
 else
-kubectl create secret generic ${CLUSTER_NAME} --from-file=${KCONF_KIND} -n ${NAMESPACE}
-kubectl label secret ${CLUSTER_NAME} istio/multiCluster=true -n ${NAMESPACE}
+kubectl create secret generic ${CLUSTER_NAME} --from-file=${KCONF_KIND} -n ${NAMESPACE} ${KCONF:+--kubeconfig $KCONF}
+kubectl label secret ${CLUSTER_NAME} istio/multiCluster=true -n ${NAMESPACE} ${KCONF:+--kubeconfig $KCONF}
 fi
+
+echo "------------Install NSM & NSC & NSE now --------"
 
