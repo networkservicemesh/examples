f8b83321acf9459fa5c0d8a15e6aa8fd9a68c79e
Radoslav Dimitrov <dimitrovr@vmware.com>
2019-10-17 16:03:53 +0300

    4G Network Topology example (#50)
    * Initial commit for 4G Network Topology example
    
    The following is an example that aims to recreate the network topology of a typical 4G network setup using Network Service Mesh
    
    Signed-off-by: Radoslav Dimitrov <dimitrovr@vmware.com>

---
+++ examples/4g-network/sidecar-nse/cmd/main.go
@@ -0,0 +1,51 @@
+// Copyright 2018 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+
+	"github.com/networkservicemesh/networkservicemesh/pkg/tools"
+	"github.com/networkservicemesh/networkservicemesh/sdk/common"
+	"github.com/networkservicemesh/networkservicemesh/sdk/endpoint"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	// Capture signals to cleanup before exiting
+	c := tools.NewOSSignalChannel()
+
+	configuration := common.FromEnv()
+	composite := endpoint.NewCompositeEndpoint(
+		endpoint.NewMonitorEndpoint(configuration),
+		endpoint.NewConnectionEndpoint(configuration),
+		endpoint.NewIpamEndpoint(configuration),
+	)
+
+	nsmEndpoint, err := endpoint.NewNSMEndpoint(context.Background(), configuration, composite)
+	if err != nil {
+		logrus.Fatalf("%v", err)
+	}
+
+	if err := nsmEndpoint.Start(); err != nil {
+		logrus.Fatalf("Unable to start the endpoint: %v", err)
+	}
+
+	defer func() { _ = nsmEndpoint.Delete() }()
+
+	// Capture signals to cleanup before exiting
+	<-c
+}
