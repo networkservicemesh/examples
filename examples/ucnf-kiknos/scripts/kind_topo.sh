9a8072b09ac4f9a3bd3e025ecc8ca1ad7e34ed7f
dvladco <vladcodaniel@gmail.com>
2020-04-27 18:07:48 +0300

    fix: prevent restart of the NSE using single node cluster

--- examples/ucnf-kiknos/scripts/kind_topo.sh
+++ examples/ucnf-kiknos/scripts/kind_topo.sh
@@ -10,6 +10,7 @@ NSE_HUB=networkservicemesh
 NSE_TAG=kiknos
 PULLPOLICY=${PULLPOLICY:-IfNotPresent}
 SERVICENAME=${SERVICENAME:-icmp-responder}
+KUBECONFDIR=${KUBECONFDIR:-~/kubeconfigdir}
 
 if [ "$1" == "--delete" ]; then
   echo "Delete clusters: $CLUSTER1, $CLUSTER2"
@@ -17,6 +18,49 @@ if [ "$1" == "--delete" ]; then
   KIND_CLUSTER_NAME=$CLUSTER2 make kind-stop
   exit 0
 fi
+
+K8S_STARTPORT=${K8S_STARTPORT:-38790}
+JAEGER_STARTPORT=${JAEGER_STARTPORT:-38900}
+
+function kindCreateCluster {
+    local name=$1; shift
+    local kconf=$1; shift
+    if [[ $# > 1 ]]; then
+        local hostip=$1; shift
+        local portoffset=$1; shift
+    fi
+
+    if [[ -n ${hostip} ]]; then
+        HOSTIP=${hostip}
+        K8S_HOSTPORT=$((${K8S_STARTPORT} + $portoffset))
+        JAEGER_HOSTPORT=$((${JAEGER_STARTPORT} + $portoffset))
+        cat <<EOF > ${KINDCFGDIR}/${name}.yaml
+kind: Cluster
+apiVersion: kind.sigs.k8s.io/v1alpha3
+kubeadmConfigPatchesJson6902:
+- group: kubeadm.k8s.io
+  version: v1beta2
+  kind: ClusterConfiguration
+  patch: |
+    - op: add
+      path: /apiServer/certSANs/-
+      value: "${HOSTIP}"
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 6443
+    hostPort: ${K8S_HOSTPORT}
+    listenAddress: ${HOSTIP}
+  - containerPort:  31922
+    hostPort: ${JAEGER_HOSTPORT}
+    listenAddress: ${HOSTIP}
+EOF
+        kind create cluster --name ${name} --config ${KINDCFGDIR}/${name}.yaml
+    else
+        kind create cluster --name ${name}
+    fi
+    kind get kubeconfig --name=${name} > ${kconf}
+}
 
 echo "Build ucnf image with kiknos base"
 VPP_AGENT=${VPP_AGENT} TAG=kiknos make k8s-universal-cnf-save &
@@ -32,13 +76,14 @@ function installNSM() {
   echo "Install NSM: $cluster"
   kubectl --context "kind-$cluster" wait -n kube-system --timeout=150s --for condition=Ready --all pods -l app=helm
   SPIRE_ENABLED=false INSECURE=true make helm-install-nsm
+  kubectl wait --context "kind-$cluster" --timeout=150s --for condition=Ready -l "app in (nsm-admission-webhook,nsmgr-daemonset,proxy-nsmgr-daemonset,nsm-vpp-plane)" -n nsm-system pod
 }
 
 function startCluster() {
   local cluster=$1; shift
 
   echo "Start kind clusters: $cluster"
-  KIND_CLUSTER_NAME=$cluster make kind-start
+  kindCreateCluster "$cluster" "$KUBECONFDIR/$cluster.kubeconfig"
 
   wait $dockerBuildProcess
   echo "Load images into: $cluster"
@@ -98,9 +143,11 @@ kubectl --context "kind-$CLUSTER2" wait -n default --timeout=150s --for conditio
 wait
 
 echo "Create hello world pods"
-helm template ./examples/ucnf-kiknos/helm/vl3_hello --set nsm.serviceName="$SERVICENAME" | kubectl --context "kind-$CLUSTER1" apply -f -
+helm template ./examples/ucnf-kiknos/helm/vl3_hello | kubectl --context "kind-$CLUSTER1" apply -f -
 helm template ./examples/ucnf-kiknos/helm/vl3_hello --set nsm.serviceName="$SERVICENAME" | kubectl --context "kind-$CLUSTER2" apply -f -
 
 kubectl --context "kind-$CLUSTER1" wait -n default --timeout=150s --for condition=Ready --all pods -l app=icmp-responder &
 kubectl --context "kind-$CLUSTER2" wait -n default --timeout=150s --for condition=Ready --all pods -l app=icmp-responder &
 wait
+
+CLUSTER1=$CLUSTER1 CLUSTER2=$CLUSTER2 SERVICENAME=$SERVICENAME ./examples/ucnf-kiknos/scripts/test_vpn_conn.sh
