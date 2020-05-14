e38084a45010ae24561a28174752b4039b25417e
mihmatache <mihmatache@luxoft.com>
2020-05-14 16:57:25 +0300

    Updated scripts based on circle CI results

--- examples/ucnf-kiknos/scripts/test_istio_vpn_conn.sh
+++ examples/ucnf-kiknos/scripts/test_istio_vpn_conn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CLUSTER1=${CLUSTER1:-cl1}
-CLUSTER2=${CLUSTER2:-cl2}
+CLUSTER1=${CLUSTER1:-kind-cl1}
+CLUSTER2=${CLUSTER2:-kind-cl2}
 
 function curl_to() {
   local cluster=$1; shift
@@ -11,19 +11,19 @@ function curl_to() {
 }
 
 helloIPs=()
-for hello in $(kubectl --context "kind-$CLUSTER1" get pods -n istio-system -l app=istio-ingressgateway -o=name); do
-  podIp=$(kubectl --context "kind-$CLUSTER1" -n istio-system exec "$hello" -c istio-proxy -- ip addr show dev nsm0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
+for hello in $(kubectl --context "$CLUSTER1" get pods -n istio-system -l app=istio-ingressgateway -o=name); do
+  podIp=$(kubectl --context "$CLUSTER1" -n istio-system exec "$hello" -c istio-proxy -- ip addr show dev nsm0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
   echo "Detected pod with nsm interface ip: $podIp"
   helloIPs+=("$podIp")
 done
 
-for hello in $(kubectl --context "kind-$CLUSTER2" get pods -l app=icmp-responder -o=name); do
+for hello in $(kubectl --context "$CLUSTER2" get pods -l app=icmp-responder -o=name); do
   for ip in "${helloIPs[@]}"; do
     echo "------------------------- Source $hello -------------------------"
-    curl_to "kind-$CLUSTER2" "http://$ip/hello"
-    curl_to "kind-$CLUSTER2" "http://$ip/hello-v2"
-    curl_to "kind-$CLUSTER2" "http://$ip:8000/hello"
-    curl_to "kind-$CLUSTER2" "http://$ip:8000/hello-v2"
+    curl_to "$CLUSTER2" "http://$ip/hello"
+    curl_to "$CLUSTER2" "http://$ip/hello-v2"
+    curl_to "$CLUSTER2" "http://$ip:8000/hello"
+    curl_to "$CLUSTER2" "http://$ip:8000/hello-v2"
     echo "-----------------------------------------------------------------------------------------------------"
   done
 done
