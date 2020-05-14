10769b8f0c58a19eb28c4dd36cea6ddb13182523
mihmatache <mihmatache@luxoft.com>
2020-05-14 15:36:39 +0300

    Updated scripts after failure on circle CI

--- examples/ucnf-kiknos/scripts/kind_topo.sh
+++ examples/ucnf-kiknos/scripts/kind_topo.sh
@@ -185,7 +185,7 @@ if [ "$ISTIO" == "true" ]; then
   echo "Installing Istio control plane"
   kubectl --context "$CLUSTER1" apply -f ./examples/ucnf-kiknos/k8s/istio_cfg.yaml
   sleep 2
-  kubectl --context "$cluster" wait -n istio-system --timeout=150s --for condition=Ready --all pods
+  kubectl --context "$CLUSTER1" wait -n istio-system --timeout=150s --for condition=Ready --all pods
   rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
   ./examples/ucnf-kiknos/scripts/start_clients.sh --cluster1="$CLUSTER1" --cluster2="$CLUSTER2" --istio_client
 else
