cab57ae445cfc889b4e09e39e44334fe87a800ec
dvladco <vladcodaniel@gmail.com>
2020-05-14 17:06:09 +0300

    feat: add aws integration to deployment scripts

--- examples/ucnf-kiknos/scripts/start_vpn.sh
+++ examples/ucnf-kiknos/scripts/start_vpn.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-CLUSTER1=${CLUSTER1:-kind-cl1}
-CLUSTER2=${CLUSTER2:-kind-cl2}
+CLUSTER=${CLUSTER:-kind-cl2}
 SERVICE_NAME=${SERVICE_NAME:-icmp-responder}
 
-nsePod=$(kubectl --context "$CLUSTER2" get pods -l "networkservicemesh.io/app=${SERVICE_NAME}" -o=name)
-echo "Starting VPN"
-kubectl --context "$CLUSTER2" exec -it "$nsePod" -- ipsec up kiknos
+nsePod=$(kubectl --context "$CLUSTER" get pods -l "networkservicemesh.io/app=${SERVICE_NAME}" -o=name)
+kubectl --context "$CLUSTER" exec -it "$nsePod" -- ipsec up kiknos
