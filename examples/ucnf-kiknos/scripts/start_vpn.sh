effd0b93700e23b3c2636ca60bc82a0e813a1eff
mihmatache <mihmatache@luxoft.com>
2020-05-14 16:02:17 +0300

    Updated scripts after failure on circle CI

--- examples/ucnf-kiknos/scripts/start_vpn.sh
+++ examples/ucnf-kiknos/scripts/start_vpn.sh
@@ -4,11 +4,11 @@ CLUSTER1=${CLUSTER1:-kind-cl1}
 CLUSTER2=${CLUSTER2:-kind-cl2}
 SERVICE_NAME=${SERVICE_NAME:-icmp-responder}
 
-echo "Retrieving IP and MAC addr of interface"
+echo "Retrieving IP addr of interface"
 POD_NAME=$(kubectl --context "$CLUSTER1" get pods -o name | grep icmp-responder | cut -d / -f 2)
 IP_ADDR=$(kubectl --context "$CLUSTER1" exec -it "$POD_NAME" -- ip addr | grep "global eth0" | grep inet | awk '{print $2}' | cut -d / -f 1)
 
-nsePod=$(kubectl --context "kind-$CLUSTER2" get pods -l "networkservicemesh.io/app=${SERVICE_NAME}" -o=name)
+nsePod=$(kubectl --context "$CLUSTER2" get pods -l "networkservicemesh.io/app=${SERVICE_NAME}" -o=name)
 kubectl --context "$CLUSTER2" exec -it "$nsePod" -- ping -c 4 "$IP_ADDR"
 echo "Starting VPN"
 kubectl --context "$CLUSTER2" exec -it "$nsePod" -- ipsec up kiknos
