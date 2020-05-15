3cfa390474af633233faf838d7e7b924d46eafc7
mihmatache <mihmatache@luxoft.com>
2020-05-15 18:00:26 +0300

    Fix for circleci context for test steps

--- examples/ucnf-kiknos/scripts/test_istio_vpn_conn.sh
+++ examples/ucnf-kiknos/scripts/test_istio_vpn_conn.sh
@@ -3,6 +3,40 @@
 CLUSTER1=${CLUSTER1:-kiknos-demo-1}
 CLUSTER2=${CLUSTER2:-kiknos-demo-2}
 
+
+print_usage() {
+  echo "$(basename "$0") - Deploy NSM Kiknos topology. All properties can also be provided through env variables
+
+NOTE: The defaults will change to the env values for the ones set.
+
+Usage: $(basename "$0") [options...]
+Options:
+  --cluster1            Name of Kind cluster one - Represents the client network            env var: CLUSTER1         - (Default: $CLUSTER1)
+  --cluster2            Name of Kind cluster two - Represents the VPN Gateway               env var: CLUSTER2         - (Default: $CLUSTER2)
+  --help -h             Help
+" >&2
+
+}
+
+for i in "$@"; do
+  case $i in
+  --cluster1=*)
+    CLUSTER1="${i#*=}"
+    ;;
+  --cluster2=*)
+    CLUSTER2="${i#*=}"
+    ;;
+  -h | --help)
+    print_usage
+    exit 0
+    ;;
+  *)
+    print_usage
+    exit 1
+    ;;
+  esac
+done
+
 function curl_to() {
   local cluster=$1; shift
   local address=$1; shift
@@ -27,3 +61,5 @@ for hello in $(kubectl --context "$CLUSTER2" get pods -l app=icmp-responder -o=n
     echo "-----------------------------------------------------------------------------------------------------"
   done
 done
+
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
