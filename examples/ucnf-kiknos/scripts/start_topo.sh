eb6a4c58ff6a55f37c24259e6db3d4dff65a9b8d
mihmatache <mihmatache@luxoft.com>
2020-05-29 19:56:15 +0300

    Updated start_topo.sh and Makefile to be able to deploy ASAv in AWS

--- examples/ucnf-kiknos/scripts/start_topo.sh
+++ examples/ucnf-kiknos/scripts/start_topo.sh
@@ -8,6 +8,7 @@ NSE_ORG=${NSE_ORG:-mmatache}
 NSE_TAG=${NSE_TAG:-kiknos}
 PULL_POLICY=${PULL_POLICY:-IfNotPresent}
 SERVICE_NAME=${SERVICE_NAME:-icmp-responder}
+AWS_KEY_PAIR=${KEY_PAIR:-kiknos-asa}
 
 # Script run information
 DELETE=${DELETE:-false}
@@ -19,6 +20,7 @@ BUILD_IMAGE=${BUILD_IMAGE:-false}
 KIND_LOAD=${KIND_LOAD:-true}
 PUSH_IMAGE=${PUSH_IMAGE:-false}
 AWS=${AWS:-false}
+DEPLOY_ASA=${DEPLOY_ASA:-false}
 DRY_RUN=${DRY_RUN:-false}
 NO_ISTIO=${NO_ISTIO:-false}
 
@@ -38,6 +40,7 @@ Options:
   --tag                 NSE image tag                                                       env var: NSE_TAG          - (Default: $NSE_TAG)
   --pull_policy         Pull policy for the NSE image                                       env var: PULL_POLICY      - (Default: $PULL_POLICY)
   --service_name        NSM service                                                         env var: SERVICE_NAME     - (Default: $SERVICE_NAME)
+  --aws_key_pair        Key Pair used for SSH on AWS EC2 instance                           env var: AWS_KEY_PAIR     - (Default: $AWS_KEY_PAIR)
   --build_image         Indicates whether the NSE image should be built or just pulled
                         from the image repository                                           env var: BUILD_IMAGE      - (Default: $BUILD_IMAGE)
   --push_image          Indicates whether the build image should be pushed
@@ -49,6 +52,7 @@ Options:
   --clean               Removes the NSEs and Clients from the clusters                      env var: CLEAN            - (Default: $CLEAN)
   --delete              Delete the Kind clusters                                            env var: DELETE           - (Default: $DELETE)
   --aws                 Creates aws clusters (requires python >= v2.7 and eksctl v0.18.0)   env var: AWS              - (Default: $AWS)
+  --deploy_asa          Creates an ASAv instance in an aws environment                      env var: DEPLOY_ASA       - (Default: $DEPLOY_ASA)
   --dry_run             Display commands instead of executing them (useful for debugging)   env var: DRY_RUN          - (Default: $DRY_RUN)
   --no_istio            Set if you do not want the istio service mesh to be deployed        env var: NO_ISTIO         - (Default: $ISTIO)
   --help -h             Help
@@ -76,11 +80,17 @@ for i in "$@"; do
   --pull_policy=*)
     PULL_POLICY="${i#*=}"
     ;;
+  --aws_key_pair=*)
+    AWS_KEY_PAIR="${i#*=}"
+    ;;
   --build_image)
     BUILD_IMAGE=true
     ;;
   --clusters_present)
     CLUSTERS_PRESENT=true
+    ;;
+  --deploy_asa)
+    DEPLOY_ASA=true
     ;;
   --clean)
     OPERATION=delete
@@ -157,8 +167,8 @@ if [ $AWS == "true" ]; then
       ref="--ref $FIRST_CLUSTER"
     fi
 
-    python ./examples/ucnf-kiknos/scripts/aws_create_cluster.py --name "$cluster" $ref --cidr "$CIDR" --open-sg
-
+    python ./examples/ucnf-kiknos/scripts/pyaws/create_cluster.py --name "$cluster" $ref --cidr "$CIDR" --open-sg
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
     aws eks update-kubeconfig --name="$cluster" --alias "$cluster"
   }
 else
@@ -168,6 +178,7 @@ else
 
     echo "# Start kind clusters: $cluster"
     kind create cluster --name "$cluster"
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
   }
 fi
 
@@ -256,15 +267,26 @@ if [ "$BUILD_IMAGE" == "true" ]; then
 fi
 
 performNSE "$CLUSTER1" $OPERATION --set strongswan.network.localSubnet=172.31.22.0/24 \
-  --set strongswan.network.remoteSubnets="{172.31.23.0/24,172.31.100.0/24}"
+  --set strongswan.network.remoteSubnets="{172.31.23.0/24,192.168.254.0/24}"
 
 echo "# Retrieving IP and MAC addr of interface"
 INTERFACE="global eth0"
 if [ $AWS == "true" ]; then
   INTERFACE="global dynamic eth0"
 fi
-POD_NAME=$(kubectl --context "$CLUSTER1" get pods -o name | grep icmp-responder | cut -d / -f 2)
+POD_NAME=$(kubectl --context "$CLUSTER1" get pods -o name | grep endpoint | cut -d / -f 2)
 IP_ADDR=$(kubectl --context "$CLUSTER1" exec -it "$POD_NAME" -- ip addr | grep "$INTERFACE" | grep inet | awk '{print $2}' | cut -d / -f 1)
+
+if [ "$DEPLOY_ASA" == "true" ] && [ "$AWS" == "true" ]; then
+  # Update ASA config file
+  day0=$(sed -e "s/<PEER_CONNECT_IP>/${IP_ADDR}/g" -e "s/<HOST_NETWORK>/192.168.254.0/g" "examples/ucnf-kiknos/scripts/day0.txt")
+  # Deploy ASA
+  python  examples/ucnf-kiknos/scripts/pyaws/create_ec2.py --name kiknos-asa --key-pair "$AWS_KEY_PAIR" \
+      --ref "$CLUSTER1"  --image-id ami-0fe62e1a9161ec45e --interface-count 2 --user-data "$day0" --interface-in-subnet "192.168.254.0/24"
+  # Deploy Ubuntu to act as client for ASA
+  python  examples/ucnf-kiknos/scripts/pyaws/create_ec2.py --name kiknos-client --key-pair "$AWS_KEY_PAIR" \
+      --ref "$CLUSTER1"  --image-id ami-07c1207a9d40bc3bd --instance-type t2.micro --interface-count 1 --interface-in-subnet "192.168.254.0/24"
+fi
 
 performNSE "$CLUSTER2" $OPERATION --set strongswan.network.remoteAddr="$IP_ADDR" \
   --set strongswan.network.localSubnet=172.31.23.0/24 \
@@ -285,5 +307,3 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 sleep 2
 CLUSTER="$CLUSTER2" SERVICE_NAME="$SERVICE_NAME" ./examples/ucnf-kiknos/scripts/start_vpn.sh
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-
